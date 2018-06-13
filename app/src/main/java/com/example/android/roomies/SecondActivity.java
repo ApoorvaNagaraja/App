ff9ea12e1bee0a6448be8a58146787e66fade730
apoorva <apoorvanagaraj95@gmail.com>
2018-06-13 14:48:54 -0700

    second commit

--- app/src/main/java/com/example/android/roomies/SecondActivity.java
+++ app/src/main/java/com/example/android/roomies/SecondActivity.java
@@ -1,13 +1,34 @@
 package com.example.android.roomies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class SecondActivity extends AppCompatActivity {
+    TextView signUp;
+    private static final int REQUEST_SIGNUP = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        signUp=(TextView)findViewById(R.id.link_signup);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+                finish();
+
+            }
+        });
     }
+
+
 }
